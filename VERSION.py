345243e9ccda593451baacbeaf58fbd2da405ef2
Christoph Boecklin <christoph.boecklin@swisstopo.ch>
2020-11-30 08:53:39 +0100

    test

--- VERSION.py
+++ VERSION.py
@@ -1,1 +1,16 @@
-VERSION='0.5.0'
+#!/usr/bin/env python
+import subprocess
+process = subprocess.Popen(["git", "tag", "--points-at", "HEAD"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+git_version = stdout.decode('utf-8').strip()
+if git_version == '':
+    proc = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    VERSION = stdout.decode('utf-8').strip()
+else:
+    VERSION = git_version
+print(VERSION)
